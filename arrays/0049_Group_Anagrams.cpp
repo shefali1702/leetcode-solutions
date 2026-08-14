bb79be26a20cd7928d4217c4ee81b783bd38aa19
Shefali Sharma <shefalisharma1702@gmail.com>
2026-08-14 11:09:18 +0530

    Added Group Anagrams Solution

---
+++ arrays/0049_Group_Anagrams.cpp
@@ -0,0 +1,31 @@
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map <string, vector<string> > mp;
+
+        int n = strs.size();   
+
+        for(int i =0; i< n;i++){
+
+        string temp = strs[i];
+        sort(temp.begin(),temp.end());
+
+        mp[temp].push_back(strs[i]);
+
+        }
+  vector<vector<string>> result;
+
+        for (auto it : mp) {
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
