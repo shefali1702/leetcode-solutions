aaa0eab1bb32762f85ccddcaea649189f5a558aa
Shefali Sharma <shefalisharma1702@gmail.com>
2026-08-21 12:52:00 +0530

    added two sum 2 solution

---
+++ arrays/0167_Two_sum_II.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+      int right = numbers.size()-1;
+      int left=0;
+
+while(left<right){
+    int sum=numbers[left]+numbers[right];
+
+    if(sum==target){
+        return {left+1,right+1};
+    }
+
+    else if(sum>target){
+        right--;
+    }
+    else{
+        left++;
+    }
+
+}
+
+return {};
+    }
+};
